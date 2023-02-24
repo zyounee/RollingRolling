0c3e0249a18d9d3628a9b02c324f2eba99673ef0
Jiyoon <51226340+trueyoon@users.noreply.github.com>
2023-02-24 16:26:17 +0900

    아이디 비밀번호 조건

--- src/main/java/com/example/hanghaeworld/entity/User.java
+++ src/main/java/com/example/hanghaeworld/entity/User.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 
 @Entity(name = "users")
 @Getter
@@ -17,8 +18,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false, unique = true)
+    @Pattern(regexp = "^[a-z\\d`]{4,10}$")
     private String username;
     @Column(nullable = false)
+    @Pattern(regexp = "^[a-zA-Z\\d`~!@#$%^&*()-_=+]{8,15}$")
     private String password;
     @Column(nullable = false)
     private String nickname;
