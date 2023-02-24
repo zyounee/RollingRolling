950dd31aa3eca59171ce97807ad329396697eda6
the1224 <asdz2003@gmail.com>
2023-02-24 16:31:33 +0900

    Jeonghwan Choi 예외처리 구현

---
+++ src/main/java/com/example/hanghaeworld/exception/ExceptionHandlers.java
@@ -0,0 +1,20 @@
+package com.example.hanghaeworld.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlers {
+
+    @ExceptionHandler(value = { IllegalArgumentException.class })
+    public ResponseEntity<Object> handleApiRequestException(IllegalArgumentException ex) {
+        RestApiException restApiException = new RestApiException();
+        restApiException.setHttpStatus(HttpStatus.BAD_REQUEST);
+        restApiException.setErrorMessage(ex.getMessage());
+
+
+        return new ResponseEntity(restApiException.getErrorMessage(), restApiException.getHttpStatus());
+    }
+}
