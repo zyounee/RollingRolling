c1586af340ddb589e147bb4fb9ba28abe116b745
the1224 <asdz2003@gmail.com>
2023-02-24 14:27:43 +0900

    Security, Config, Entity, build.gradle, application.properties 세팅

--- src/main/java/com/example/hanghaeworld/config/WebSecurityConfig.java
+++ src/main/java/com/example/hanghaeworld/config/WebSecurityConfig.java
@@ -1,54 +1,55 @@
-//package com.example.hanghaeworld.config;
-//
-//
-//
-////import com.example.hanghaeworld.jwt.JwtUtil;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//@Configuration
-//@RequiredArgsConstructor
-//@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
-//@EnableGlobalMethodSecurity(securedEnabled = true) // @Secured 어노테이션 활성화
-//public class WebSecurityConfig {
-//
-//    private final JwtUtil jwtUtil;
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        // h2-console 사용 및 resources 접근 허용 설정
-//        return (web) -> web.ignoring()
-//                .requestMatchers(PathRequest.toH2Console())
-//                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        // CSRF 설정
-//        http.csrf().disable();
-//
-//        http.authorizeRequests().antMatchers("/api/user/**").permitAll()
-//                .antMatchers("/api/memos/**").permitAll()
-//                .antMatchers().authenticated()
-//                .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
-//
-//        // 특정 게시글 조회, 삭제, 수정 : "api/memos/{id}"
-//        // 게시글 전체 조회, 작성 : "/api/memos"
-//
-//
-//        http.authorizeRequests().anyRequest().authenticated();
-//
-//        // 로그인 사용
-//        http.formLogin();
-//
-//        return http.build();
-//    }
-//
-//}
+package com.example.hanghaeworld.config;
+
+
+
+import com.example.hanghaeworld.jwt.JwtAuthFilter;
+import com.example.hanghaeworld.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+@EnableGlobalMethodSecurity(securedEnabled = true) // @Secured 어노테이션 활성화
+public class WebSecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // h2-console 사용 및 resources 접근 허용 설정
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console())
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정
+        http.csrf().disable();
+
+        http.authorizeRequests().antMatchers("/api/user/**").permitAll()
+                .antMatchers("/api/memos/**").permitAll()
+                .antMatchers().authenticated()
+                .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        // 특정 게시글 조회, 삭제, 수정 : "api/memos/{id}"
+        // 게시글 전체 조회, 작성 : "/api/memos"
+
+
+        http.authorizeRequests().anyRequest().authenticated();
+
+        // 로그인 사용
+        http.formLogin();
+
+        return http.build();
+    }
+
+}
