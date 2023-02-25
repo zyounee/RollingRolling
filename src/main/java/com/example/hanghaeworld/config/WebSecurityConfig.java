52c3e5b961372c53bc8a4a7f2d58e48409a42906
jenny <jennykim6890@naver.com>
2023-02-25 21:32:01 +0900

    먼저가보겠습니다

--- src/main/java/com/example/hanghaeworld/config/WebSecurityConfig.java
+++ src/main/java/com/example/hanghaeworld/config/WebSecurityConfig.java
@@ -26,7 +26,6 @@ public class WebSecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         // h2-console 사용 및 resources 접근 허용 설정
         return (web) -> web.ignoring()
-                .requestMatchers(PathRequest.toH2Console())
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
