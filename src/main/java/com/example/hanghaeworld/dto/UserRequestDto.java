feb33cd0c644a658e9f441eaa4246b5ccd1b32e3
the1224 <asdz2003@gmail.com>
2023-02-27 14:25:08 +0900

    유저 정보 변경 기능 구현

--- src/main/java/com/example/hanghaeworld/dto/UserRequestDto.java
+++ src/main/java/com/example/hanghaeworld/dto/UserRequestDto.java
@@ -3,6 +3,7 @@ package com.example.hanghaeworld.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Getter
@@ -10,8 +11,11 @@ import javax.validation.constraints.Pattern;
 public class UserRequestDto {
     private String image;
     private String introduction;
+    @NotNull
     private String currentPassword;
     @Pattern(regexp = "(?=.*?[a-zA-Z])(?=.*?[\\d])(?=.*?[~!@#$%^&*()_+=\\-`]).{8,15}")
     private String newPassword;
+    private String newPasswordConfirm;
+    @NotNull
     private String nickname;
 }
